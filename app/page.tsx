5eb128fe35e551c232ae454ec6c9533b05c5ea96
Mher119 <korkotyanmher1994@gmail.com>
2025-12-06 21:21:58 +0400

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
+import styles from "./Home.module.css";
+import Image from "next/image";
 
 
 export default function Home() {
   return (
-    
-  );
+    <div className={styles.container}>
+      
+      <div className={styles.main}>
+        <h1>divine softness</h1>
+        <p>our heritage</p>
+      </div>
+
+      <div className={styles.card}>
+        <p className={styles.about}>About</p>
+        <p>At Sacre Jewellery, we believe that beauty is energy — unseen, yet deeply felt. Our creations are born from the fusion of craftsmanship and spirit. Each design is infused with intention, balancing elegance and mysticism.</p>
+       
+        <div className={styles.imgColumn}>
+        <Image src="/Group 59.png" alt="image" width={263} height={310} className={styles.img1}/>
+        <div className={styles.learn}>Learn more</div>
+        </div>
+        
+        <Image src="/image 68.png" alt="image" width={655} height={795} className={styles.img2}/>
+       
+
+      </div>
+
+    </div>
+  )
 }
+
+
